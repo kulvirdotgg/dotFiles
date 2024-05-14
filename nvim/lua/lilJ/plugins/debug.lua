508ef5e9312625dde8ad252020dc647360bf8c62
Kulvir Singh <kulvirs734@gmail.com>
2024-05-14 22:18:58 +0530

    dap ui

--- nvim/lua/lilJ/plugins/debug.lua
+++ nvim/lua/lilJ/plugins/debug.lua
@@ -22,20 +22,12 @@ return {
         local dapui = require "dapui"
 
         require("mason-nvim-dap").setup {
-            -- Makes a best effort to setup the various debuggers with
-            -- reasonable debug configurations
             automatic_setup = true,
 
             automatic_installation = true,
 
-            -- You can provide additional configuration to the handlers,
-            -- see mason-nvim-dap README for more information
             handlers = {},
-
-            -- You'll need to check that you have the required things installed
-            -- online, please don't ask me how to install them :)
             ensure_installed = {
-                -- Update this to ensure that you have the debuggers for the langs you want
                 "delve",
             },
         }
@@ -53,8 +45,9 @@ return {
         -- Dap UI setup
         -- For more information, see |:help nvim-dap-ui|
         dapui.setup {
-            icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
             controls = {
+                element = "repl",
+                enabled = true,
                 icons = {
                     pause = "⏸",
                     play = "▶",
@@ -66,6 +59,70 @@ return {
                     terminate = "⏹",
                     disconnect = "⏏",
                 },
+            },
+            element_mappings = {},
+            expand_lines = true,
+            floating = {
+                border = "single",
+                mappings = {
+                    close = { "q", "<Esc>" },
+                },
+            },
+            icons = {
+                expanded = "▾",
+                collapsed = "▸",
+                current_frame = "*",
+            },
+            force_buffers = true,
+            layouts = {
+                {
+                    elements = {
+                        {
+                            id = "scopes",
+                            size = 0.25,
+                        },
+                        {
+                            id = "breakpoints",
+                            size = 0.25,
+                        },
+                        {
+                            id = "stacks",
+                            size = 0.25,
+                        },
+                        {
+                            id = "watches",
+                            size = 0.25,
+                        },
+                    },
+                    position = "left",
+                    size = 40,
+                },
+                {
+                    elements = {
+                        {
+                            id = "repl",
+                            size = 0.5,
+                        },
+                        {
+                            id = "console",
+                            size = 0.5,
+                        },
+                    },
+                    position = "bottom",
+                    size = 10,
+                },
+            },
+            mappings = {
+                edit = "e",
+                expand = { "<CR>", "<2-LeftMouse>" },
+                open = "o",
+                remove = "d",
+                repl = "r",
+                toggle = "t",
+            },
+            render = {
+                indent = 1,
+                max_value_lines = 100,
             },
         }
 
