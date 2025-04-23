742011ae6ddf27b9ff340c7b0783836c3616df25
Kulvir Singh <kulvirs734@gmail.com>
2025-04-23 23:18:59 +0530

    removed deprecated funcs and formatting correction

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -34,8 +34,14 @@ vim.opt.hlsearch = true
 set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+set("n", "[d", function()
+    vim.diagnostic.jump { count = -1, float = true }
+end, { desc = "Go to previous [D]iagnostic message" })
+
+set("n", "]d", function()
+    vim.diagnostic.jump { count = 1, float = true }
+end, { desc = "Go to next [D]iagnostic message" })
+
 set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 
