331939657c7867a70a113cdfdefecc6cdbb32a48
Kulvir Singh <kulvirs734@gmail.com>
2025-05-15 17:33:37 +0530

    edited install scripts

--- install.sh
+++ install.sh
@@ -1,72 +1,21 @@
-#!/bin/zsh
+#!/bin/bash
 
 # ---- Terminal ----
+
 echo "Installing your Teminal and other thingies..."
-brew install git
-brew install ghostty
-brew install starship
-brew install neovim
-brew install font-jetbrains-mono-nerd-font
-brew install tmux
-brew install fzf
-brew install lsd
-brew install ripgrep
-brew install zoxide
-brew install lazygit
-brew install fastfetch
-brew install btop
-brew install zsh-syntax-highlighting
-brew install stow
+yay -S git ghostty starship neovim ttf-jetbrains-mono-nerd tmux fzf lsd ripgrep zoxide lazygit fastfetch stow btop
 
 echo "Creating symlinks using GNU STOW"
 stow .
 
-echo "Sourcing zsh config It should work now atleast"
+echo "changing default shell to z-shell"
+sudo chsh -s $(which zsh)
 source ~/.zshrc
 
-echo "Installing languages & runtime envs etc..."
-brew install oven-sh/bun/bun
-brew install go
-brew install node
-brew install pnpm
+yay -S bun-bin go node npm
 
 # ---- Applications ----
 echo "Installing the Applications..."
-brew install --cask discord
-brew install --cask docker
-brew install --cask ghostty
-brew install --cask httpie
-brew install --cask keka
-brew install --cask obsidian
-brew install --cask raycast
-brew install --cask spotify
-brew install --cask zen-browser
-
-
-yabai() {
-    brew install koekeishiya/formulae/yabai
-    brew install koekeishiya/formulae/skhd
-    brew install FelixKratz/formulae/borders
-    brew install FelixKratz/formulae/sketchybar
-    
-    yabai --start-service
-    skhd --start-service
-    brew services start borders
-    brew services start sketchybar
-}
-
-install_yabai=false
-
-for arg in "$@"; do
-    if [ "$arg" = "--yabai" ]; then
-        install_yabai=true
-        break
-    fi
-done
-
-if $install_yabai; then
-    echo "Installing yabaiiiiiiiii...."
-    yabai
-fi
+yay -S zen-browser-bin discord docker spotify
 
 echo "setup complete...\n"
